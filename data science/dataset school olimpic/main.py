bdbdb3f30ea2b88f543f62827d29fd290f94c8bd
Holms <samiycrutoyden@gmail.com>
2022-09-30 00:07:01 +0300

    + rating

--- data science/dataset school olimpic/main.py
+++ data science/dataset school olimpic/main.py
@@ -5,7 +5,7 @@ from flask import request, render_template, Flask
 app = Flask('MAIN', template_folder=os.getcwd())
 data_connect = {'dbname':'MIIT','user':'postgres','host':'localhost','password':''} # Коннект к базе
 
-@app.route('/', methods=('GET','POST'))
+@app.route('/rating/olimp', methods=('GET','POST'))
 def main():
     db = psycopg2.connect(**data_connect)
     sql = db.cursor()
@@ -21,32 +21,46 @@ def main():
         year = "(" + ''.join([f"year='{r}' OR " for r in data.getlist('year') if r != 'all'])[:-3]+')'
         subject =  "(" + ''.join([f"subject='{r}' OR " for r in data.getlist('subject') if r != 'all'])[:-3]+')'
         status =  "(" + ''.join([f"status='{r}' OR " for r in data.getlist('status') if r != 'all'])[:-3]+')'
-
-        sql.execute(f"""
-        SELECT shortname, count(shortname) 
+        print(year)
+        print(subject)
+        print(status)
+        q = f"""
+        SELECT fullname, count(fullname) 
         FROM schools 
         {'WHERE' if year != '()' or subject != '()' or status != '()' else ''}
         {year if year  != '()' else '' }
-        {'AND' if subject != '()' else ''}
+        {'AND' if year != '()' and (subject != '()' or status != '()') else ''}
         {subject if subject  != '()' else ''}
-        {'AND' if status != '()' else ''}
+        {'AND' if subject != '()' and status !='()' else ''}
         {status if status != '()' else ''} 
-        GROUP BY LOWER(REPLACE(REPLACE(REPLACE(shortname,' ',''),'»',''),'«','')), shortname 
-        ORDER BY COUNT(shortname ) {'DESC' if data.get('is_desc') else ''} 
-        LIMIT 100""")
+        GROUP BY LOWER(REPLACE(REPLACE(REPLACE(fullname,' ',''),'»',''),'«','')), fullname 
+        ORDER BY COUNT(fullname ) {'DESC' if data.get('is_desc') else ''} 
+        LIMIT 100"""
+        print(q)
+        sql.execute(q)
         rows = sql.fetchall()
 
     else:
         sql.execute(f"""
-        SELECT shortname, count(shortname) 
+        SELECT fullname, count(shortname) 
         FROM schools 
-        GROUP BY LOWER(REPLACE(REPLACE(REPLACE(shortname,' ',''),'»',''),'«','')), shortname 
-        ORDER BY COUNT(shortname ) DESC 
+        GROUP BY LOWER(REPLACE(REPLACE(REPLACE(fullname,' ',''),'»',''),'«','')), fullname 
+        ORDER BY COUNT(fullname ) DESC 
         LIMIT 100""")
         rows = sql.fetchall()
 
     db.close()
-    return render_template('index.html', years=years, subjects=subjects, rows=rows, data_year=data.getlist('year'),data_subject=data.getlist('subject'),data_status=data.getlist('status'))
+    return render_template('olimp.html', years=years, subjects=subjects, rows=rows, data_year=data.getlist('year') or ('all'),data_subject=data.getlist('subject') or ('all'),data_status=data.getlist('status') or ('all'))
 
-webbrowser.open("http://localhost", new=1)
+@app.route('/rating/exams', methods=('GET','POST'))
+def rating():
+    db = psycopg2.connect(**data_connect)
+    sql = db.cursor()
+
+    sql.execute("""SELECT CAST(SUM(passes_over_220*3 + passer_under_160*2 + oge_score)/50 AS numeric(6,2)) as balls, ege.edu_name FROM ege INNER JOIN oge ON ege.edu_name = oge.edu_name  AND oge.year=ege.year GROUP BY LOWER(REPLACE(REPLACE(REPLACE(ege.edu_name,' ',''),'»',''),'«','')), ege.edu_name  ORDER BY balls DESC LIMIT 100;""")
+    exams = sql.fetchall()  # EGE raiting
+    return render_template('exams.html', rows=exams)
+
+
+webbrowser.open("http://localhost/rating/olimp", new=1)
 app.run('0.0.0.0', port=80, debug=False)
